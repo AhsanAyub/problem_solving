9c15fbca092672383cc5bf538df06d8429f60c8f
Ahsan Ayub <md.ahsanayub@outlook.com>
2020-07-09 00:27:30 -0500

    Initialize folder for stack and queue

---
+++ Stack_and_Queue/queue.cpp
@@ -0,0 +1,16 @@
+/*
+ * Implementation of Queue in C++
+ *
+ * A queue implements FIFO (first-in first-out) ordering.
+ * As in a line or queue at a ticket stand, items are removed
+ * from the data structure in the same order that they are added.
+ *
+ * Following operations are included in the Queue -
+ *
+ * add(item): Add an item to the end of the list.
+ * remove(): Remove the first item in the list.
+ * peek() : Return the top of the queue.
+ * isEmpty() : Return true if and only if the queue is empty.
+ * 
+ * Last Edited on July 9, 2020 00:30:45 (CST)
+ */
---
+++ Stack_and_Queue/stack.cpp
@@ -0,0 +1,16 @@
+/*
+ * Implementation of Stack in C++
+ *
+ * A stack uses LIFO (last-in first-out) ordering.
+ * That is, as in a stack of dinner plates, the most recent
+ * item added to the stack is the first item to be removed.
+ *
+ * Following operations are included in the Stack -
+ *
+ * pop() : Remove the top item from the stack.
+ * push(item) : Add an item to the top of the stack.
+ * peek() : Return the top of the stack.
+ * isEmpty() : Return true if and only if the stack is empty.
+ * 
+ * Last Edited on July 9, 2020 00:30:45 (CST)
+ */
