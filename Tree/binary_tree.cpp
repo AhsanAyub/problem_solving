24e7df1ac83fcc7755ced4d8d2611bcfc607db64
Ahsan Ayub <md.ahsanayub@outlook.com>
2021-03-01 02:31:28 -0600

    Added some functionalities of BST

--- Tree/binary_tree.cpp
+++ Tree/binary_tree.cpp
@@ -6,21 +6,23 @@
  * Insertion (as per the binary search tree formation),
  * Lookup (search a "target" value), Deletion (Delete
  * a node from the tree following BST structure),
- * and Traversal (inorder, preorder, and postorder).
+ * and Traversal (inorder, preorder, postorder, and level order).
  *
  * Other functionalites include:
  *      (1) size (count the number of the nodes in the tree)
  *      (2) maxDepth (computation of the number of the nodes along with the
  *          longest path from the root node down to the farthest leaf node)
- *      (3) minValue (return the minimum value of the BST)
- *      (4) mirror (Roles of the left and right pointers are swapped at every node)
- *      (5) doubleTree (create a new duplicate node in a BST)
- *      (6) isBST (check if the tree is a BST)
+ *      (3) maxValue (return the maximum value of the BST)     
+ *      (4) minValue (return the minimum value of the BST)
+ *      (5) mirror (Roles of the left and right pointers are swapped at every node)
+ *      (6) doubleTree (create a new duplicate node in a BST)
+ *      (7) isBST (check if the tree is a BST)
  * 
  * Last Edited on February 08, 2021 00:00:00 (CST)
  */
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -91,6 +93,34 @@ TreeNode* insert(TreeNode* root, int val)
     return root; // The pointer has been unchanged throughout the operations
 }
 
+// This is the utility function to search and return the maximum value of the BST
+int maxValue(TreeNode* node)
+{
+    // Keep traversing to the right most side of the BST
+    while(true)
+    {
+        if(node->right != NULL)
+            node = node->right;
+        else
+            break;
+    }
+    return node->val;
+}
+
+// This is the utility function to search and return the minimum value of the BST
+int minValue(TreeNode* node)
+{
+    // Keep traversing to the left most side of the BST
+    while(true)
+    {
+        if(node->left != NULL)
+            node = node->left;
+        else
+            break;
+    }
+    return node->val;
+}
+
 // This is the utility function to print the preorder tree traversal
 void printPreorder(TreeNode* root)
 {
@@ -127,6 +157,34 @@ void printPostorder(TreeNode* root)
     cout << root->val << " ";     // Print value
 }
 
+// This is the utility function to print the level order tree traversal
+//TreeNode*  printLeverOrder(TreeNode* root)
+void printLeverOrder(TreeNode* root)
+{
+    // Create a queue to insert the nodes left to explore
+    queue<TreeNode*> nodeRecords;
+    
+    // A temporary node to iterate through the children of the node
+    struct TreeNode* temp = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    nodeRecords.push(root);     // initialize the queue with the root
+    
+    // Begin with the root inside of the queue and finish when the queue is empty
+    while(!nodeRecords.empty())
+    {
+        // Print the visited node that has traversed first
+        cout << nodeRecords.front()->val << " ";
+         
+        temp = nodeRecords.front(); // Point at the node
+        nodeRecords.pop();          // Remove from the queue as it's been printed
+
+        // At each iteration, we add the left and right child of the current node
+        if (temp->left != NULL)
+            nodeRecords.push(temp->left);
+        if (temp->right != NULL)
+            nodeRecords.push(temp->right);
+     }
+}
+
 // This is the utility function to compute the number of the nodes in the tree
 int treeSize(TreeNode* root)
 {
@@ -134,26 +192,24 @@ int treeSize(TreeNode* root)
         return 0;
     else
         /* Traversing in both directions and for each node visited, we will
-        get 1 returned. Hence, the root will return the sum of all the nodes. */
+        get 1 returned for node visited. Hence, the root will return the sum of all the nodes. */
         return (treeSize(root->left) + treeSize(root->right) + 1);
 }
 
 /* Given a binary search tree and a "target" value, this function searches
 the tree to see if it contains the target */
-bool lookup(TreeNode* root, int target)
+bool lookup(TreeNode* node, int target)
 {
-    if (root == NULL)   // Base condition
+    if (node == NULL)   // Base condition
         return false;
-    else
-    {
-        if (root->val == target) // Target is found
-            return true;
-        else // Keep exploring to find the target
-        {
-            if (root->val >= target)
-                
-        }
-    }
+    
+    // Keep exploring to find the target
+    if (node->val < target)         // Go left
+        lookup(node->left, target);
+    else if (node->val < target)    // Go right
+        lookup(node->right, target);
+    else                            // Target is found
+        return true;
 }
 
 // Driver program
@@ -161,18 +217,20 @@ int main()
 {
     // Creating the root node
     struct TreeNode* root = (struct TreeNode*) malloc(sizeof(struct TreeNode));
-    root->val = 10;
+    root->val = 7;
     root->left = NULL;
     root->right = NULL;
 
     cout << "Inserting the nodes." << endl;
     // Inserting some more nodes to the tree to get things started
     root = insert(root, 5);
-    root = insert(root, 1);
-    root = insert(root, 15);
+    root = insert(root, 20);
+    root = insert(root, 4);
+    root = insert(root, 6);
+    root = insert(root, 33);
 
     /*cout << root->left->left->val << endl;
-    cout << root->left->val << endl;
+    cout << root->left->right->val << endl;
     cout << root->right->val << endl;
     cout << root->val << endl;*/
 
@@ -185,7 +243,16 @@ int main()
     cout << "\nPostorder Traversal:\n";
     printPostorder(root);
 
+    cout << "\nLevel Order Traversal:\n";
+    printLeverOrder(root);
+
     cout << "\nTree Size: " << treeSize(root) << endl;
+
+    if(lookup(root, 6))
+        cout << "6 node exists" << endl;
+
+    cout << "Max Value: " << maxValue(root) << endl;
+    cout << "Min Value: " << minValue(root) << endl;
 
     return 0;
 }
