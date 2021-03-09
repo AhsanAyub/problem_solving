43017dec270b7270abc2f35b7c4929b4ee7dc5ec
Ahsan Ayub <md.ahsanayub@outlook.com>
2021-03-08 19:51:48 -0600

    Added binary search tree stable version update

--- Tree/binary_tree.cpp
+++ Tree/binary_tree.cpp
@@ -14,11 +14,9 @@
  *          longest path from the root node down to the farthest leaf node)
  *      (3) maxValue (return the maximum value of the BST)     
  *      (4) minValue (return the minimum value of the BST)
- *      (5) mirror (Roles of the left and right pointers are swapped at every node)
- *      (6) doubleTree (create a new duplicate node in a BST)
- *      (7) isBST (check if the tree is a BST)
+ *      (5) isBST (check if the tree is a BST)
  * 
- * Last Edited on February 08, 2021 00:00:00 (CST)
+ * Last Edited on March 08, 2021 19:49:28 (CST)
  */
 
 #include <iostream>
@@ -94,7 +92,7 @@ TreeNode* insert(TreeNode* root, int val)
 }
 
 // This is the utility function to search and return the maximum value of the BST
-int maxValue(TreeNode* node)
+TreeNode* maxValue(TreeNode* node)
 {
     // Keep traversing to the right most side of the BST
     while(true)
@@ -104,11 +102,11 @@ int maxValue(TreeNode* node)
         else
             break;
     }
-    return node->val;
+    return node;
 }
 
 // This is the utility function to search and return the minimum value of the BST
-int minValue(TreeNode* node)
+TreeNode* minValue(TreeNode* node)
 {
     // Keep traversing to the left most side of the BST
     while(true)
@@ -118,7 +116,7 @@ int minValue(TreeNode* node)
         else
             break;
     }
-    return node->val;
+    return node;
 }
 
 // This is the utility function to print the preorder tree traversal
@@ -200,17 +198,158 @@ int treeSize(TreeNode* root)
 the tree to see if it contains the target */
 bool lookup(TreeNode* node, int target)
 {
-    if (node == NULL)   // Base condition
+    if (node == NULL)               // Base condition
         return false;
-    
-    // Keep exploring to find the target
-    if (node->val < target)         // Go left
-        lookup(node->left, target);
-    else if (node->val < target)    // Go right
-        lookup(node->right, target);
-    else                            // Target is found
+    else if(node->val == target)    // Target is found
         return true;
+    else                            // Keep exploring to find the target
+    {
+        if (node->val < target)         // Go left
+            lookup(node->left, target);
+        else if (node->val < target)    // Go right
+            lookup(node->right, target);
+    }
 }
+
+/*
+    Delete a node from a binary search tree (BST) is done in two steps:
+    - Find the element we wish to remove (if it exits)
+    - Replace the node we want to remove with its successor (if any) to maintain
+    the BST variant (which is the left subtree elements < the right subtree elements).
+*/
+TreeNode* deleteNode(TreeNode* root, int val)
+{
+    // Check if there is any node in the tree
+    if(root == NULL)
+    {
+        cout << "The tree is empty." << endl;
+        return NULL;
+    }
+
+    // Check if the tree has only root node
+    if(root->left == NULL && root->right == NULL)
+    {
+        cout << "Only one node in the tree" << endl;
+        root = NULL;
+        return root;
+    }
+
+    struct TreeNode* targetNodeParent = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    struct TreeNode* targetNode = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+
+    // Assign both to the same point
+    targetNode = root;
+    targetNodeParent = root;
+
+    bool isFound = false;   // A flag value to determine if the node is found
+
+    // The loop to discover the target node as well as its parent for futher operations
+    while(targetNode != NULL)
+    {
+        if (targetNode->val == val)
+        {
+            isFound = true;
+            break;
+        }
+        else
+        {
+            targetNodeParent = targetNode;  // Assignment before traversing to the child node
+            if(targetNode->val > val)
+                targetNode = targetNode->left;
+            else
+                targetNode = targetNode->right;
+        }
+    }
+
+    if(!isFound) return root;    // The target node is NOT found; no further operation is required.
+
+    // Case 1: The target node is the leaf node
+    if(targetNode->left == NULL && targetNode->right == NULL)
+    {
+        cout << "Case 1: The target node is the leaf node" << endl;
+        if(targetNodeParent->left != NULL)
+        {
+            if(targetNodeParent->left->val == targetNode->val)  // Check if the child is in the left
+            {
+                targetNodeParent->left = NULL;
+                return root;
+            }
+            if(targetNodeParent->right->val == targetNode->val)  // Check if the child is in the right
+            {
+                targetNodeParent->right = NULL;
+                return root;
+            }
+        }
+        else                                                // The child is in the right
+            targetNodeParent->right = NULL;
+    }
+    // Case 2: The target node has either a left or a right subtree
+    else if(targetNode->left == NULL || targetNode->right == NULL)
+    {
+        cout << "Case 2: The target node has either a left or a right subtree" << endl;
+        // targetNode will point to its only child
+        if(targetNodeParent->left->val == targetNode->val)
+        {
+            if(targetNode->left == NULL)
+                targetNode = targetNode->right;
+            else
+                targetNode = targetNode->left;
+            targetNodeParent->left = targetNode;            
+        }
+        else
+        {
+            if(targetNode->left == NULL)
+                targetNode = targetNode->right;
+            else
+                targetNode = targetNode->left;
+            targetNodeParent->right = targetNode; 
+        }
+    }
+    // Case 3: Target node has both a left and a right subtree
+    else
+    {
+        cout << "Case 3: Target node has both a left and a right subtree" << endl;
+        // The successor can either be the largest value in the left subtree (this is implemented) 
+        // or the smallest value in the right subtree.
+        targetNodeParent = targetNode;              // No longer need to worry about the parent
+        targetNode = maxValue(targetNode->left);    // find the larget value in the left subtree
+        cout << "Swap Values: " << targetNodeParent->val << " " << targetNode->val << endl;
+        targetNodeParent->val = targetNode->val;    // Assignment is done
+
+        // Check if the left susbtree has only one node; then no need to do any recusive call
+        if(targetNodeParent->left->left == NULL && targetNodeParent->left->right == NULL)
+            targetNodeParent->left = NULL;
+        else
+            // Now, let's delete the node with this recusive call which value has just been swapped.
+            deleteNode(targetNodeParent->left, targetNodeParent->val);
+    }
+
+    return root;
+}
+
+
+/* This is the utility function that leverages maxValue() and minValue() functions
+ to check if the given tree obtains the BST property */
+ bool isBST(TreeNode* node)
+ {
+    // Base condition
+    if(node == NULL) return true;
+
+    // The maximum value of the left subtree should not be more than the node value
+    if(node->left != NULL && maxValue(node->left)->val > node->val)
+        return false;
+
+    // The minimum value of the right subtree should not be less than the node value
+    if(node->right != NULL && minValue(node->right)->val < node->val)
+        return false;
+
+    // Call both subtrees to the left and right to check the same properties
+    if(!isBST(node->left) || !isBST(node->right))
+        return false;
+
+    // Successfully passes all the checks
+    return true;
+ }
 
 // Driver program
 int main()
@@ -227,7 +366,7 @@ int main()
     root = insert(root, 20);
     root = insert(root, 4);
     root = insert(root, 6);
-    root = insert(root, 33);
+    root = insert(root, 22);
 
     /*cout << root->left->left->val << endl;
     cout << root->left->right->val << endl;
@@ -251,8 +390,16 @@ int main()
     if(lookup(root, 6))
         cout << "6 node exists" << endl;
 
-    cout << "Max Value: " << maxValue(root) << endl;
-    cout << "Min Value: " << minValue(root) << endl;
+    cout << "Max Value: " << maxValue(root)->val << endl;
+    cout << "Min Value: " << minValue(root)->val << endl;
+
+    if(isBST(root))
+        cout << "The tree has the Binary Search Tree (BST) properties." << endl;
+    else
+        cout << "The tree does not have the Binary Search Tree (BST) properties." << endl;
+
+    root = deleteNode(root, 7);
+    printInorder(root);
 
     return 0;
 }
